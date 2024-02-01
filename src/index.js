d4caaf9a3e32b52ceadb8b138eb0cc1da3ec0927
Branden Basche <brandenbasche@gmail.com>
2024-01-31 21:09:36 -0500

    moving on to asteroid collission

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-
 fetch('https://api.nasa.gov/planetary/apod?api_key=hrVSbl9GRkS9mNuy1pungArLHuYovWJg5eXHAf5w')
     .then(response => response.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.url})`;
     })
     .catch(error => console.error('Error:', error));
+
+// check if 
+
