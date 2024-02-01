d265d5b091df1b4ae883706bd7200f41f235444c
devincarrick <devin.carrick@gmail.com>
2024-01-31 20:12:23 -0500

    Add background image from NASA API

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=hrVSbl9GRkS9mNuy1pungArLHuYovWJg5eXHAf5w')
+    .then(response => response.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.url})`;
+    })
+    .catch(error => console.error('Error:', error));
